ad26c8ac9b9edfcf276065950d5a26c46cfbc8bb
Hamard <louis.hamard@wanadoo.fr>
2024-03-26 15:17:22 +0100

    Commit du header

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,41 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+
+      <header>
+          <nav className="flex items-center justify-between flex-wrap bg-gray-800 p-6 fixed w-full z-10 top-0">
+            <div className="flex items-center flex-shrink-0 text-white mr-6">
+              <a className="text-white no-underline hover:text-white hover:no-underline space-x-20" href="#">
+                <Image className="-mt-5" src="/iconesite.png" width={50} height={30} alt="iconesite"/>
+                <p className="text-center pl-5 text-4xl z-10 -mt-12">OOOH LA FLASH LONG</p>
+              </a>
+            </div>
+<div className="block lg:hidden">
+              <button id="nav-toggle" className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-white hover:border-white">
+                <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+              </button>
+            </div>
+
+            <div className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block pt-6 lg:pt-0" id="nav-content">
+              <ul className="list-reset lg:flex justify-end flex-1 items-center">
+                <li className="mr-3">
+                  <a className ="inline-block py-2 px-4 text-white no-underline" href="#">Accueil</a>
+                </li>
+                <li className="mr-3">
+                  <a className="inline-block text-gray-600 no-underline hover:text-yellow-400 hover:text-underline py-2 px-4" href="#">Auteurs</a>
+                </li>
+                <li className="mr-3">
+                  <a className="inline-block text-gray-600 no-underline hover:text-yellow-400 hover:text-underline py-2 px-4" href="#">Dashboard</a>
+                </li>
+                <li className="mr-3">
+                  <a className="inline-block text-gray-600 no-underline hover:text-yellow-400 hover:text-underline py-2 px-4 mr-10" href="#">Blog</a>
+                </li>
+                <Image className="-mt-5 mt-0 mr-5" src="/userpng.png  " width={50} height={30} alt="user"/>
+
+              </ul>
+            </div>
+          </nav>
+        </header>
         
         <p>Layout.tsx</p>
         
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+  import Image from "next/image";
 import "../src/components/footer"
 
 export default function Home() {
